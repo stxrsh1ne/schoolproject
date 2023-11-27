df84d01448967c6614ae1c600ae97ee78b226c52
stxrsh1ne <iaroslav.iliushkin@gmail.com>
2023-11-27 19:30:21 +0300

    start

---
+++ hopeend/hopeend/hopeendApp.swift
@@ -0,0 +1,17 @@
+//
+//  hopeendApp.swift
+//  hopeend
+//
+//  Created by Heads and Hands on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct hopeendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
